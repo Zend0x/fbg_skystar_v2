f6656f45d94ac177e3814b6201b05a745d236a2e
Zend0x <dwknerwow@gmail.com>
2023-05-20 21:15:10 +0200

    feat(index): added navbar, form and some basic styling

--- src/vistas/index.php
+++ src/vistas/index.php
@@ -1,15 +1,64 @@
-<?php
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../../css/index.css">
+    <title>Skystar Airways</title>
+    <?php
+        ini_set('display_errors', 1);
+        ini_set('html_errors', 1);
 
-    ini_set('display_errors', 1);
-    ini_set('html_errors', 1);
+        require("../negocio/aeropuertosReglasNegocio.php");
 
-    require("../negocio/aeropuertosReglasNegocio.php");
-
-    $aeropuertoBL=new AeropuertosReglasNegocio();
-    $datosAeropuertos=$aeropuertoBL->obtener();
-
-    foreach($datosAeropuertos as $aeropuerto){
-        echo '<p>'.$aeropuerto->getICAO().'</p>';
-    }
-
-?>
+        $aeropuertoBL=new AeropuertosReglasNegocio();
+        $datosAeropuertos=$aeropuertoBL->obtener();
+    ?>
+</head>
+<body>
+    <nav class="navbar">
+    <div class="navbar-center">
+        <span class="navbar-brand">Skystar Airways</span>
+    </div>
+    <div class="navbar-right">
+        <button class="login-button">Iniciar sesión</button>
+    </div>
+    </nav>
+    <div class="search">
+        <form action="" method="post">
+            <div class="airport-selectors">
+                <select name="dep_apt" id="dep_apt">
+                <option value="null" disabled selected>Seleccione un aeropuerto de salida</option>
+                <?php
+                    foreach($datosAeropuertos as $aeropuerto){
+                    echo '<option value="'.$aeropuerto->getICAO().'">'.$aeropuerto->getICAO().'</option>';
+                    }
+                ?>
+                </select>
+                <select name="arr_apt" id="arr_apt">
+                <option value="null" disabled selected>Seleccione un aeropuerto de llegada</option>
+                <?php
+                    foreach($datosAeropuertos as $aeropuerto){
+                    echo '<option value="'.$aeropuerto->getICAO().'">'.$aeropuerto->getICAO().'</option>';
+                    }
+                ?>
+                </select>
+            </div>
+            <div class="date-selectors-search">
+                <div class="dep-arrival">
+                    <div class="departure">
+                        <label for="fecha-salida">Fecha de salida:</label><br>
+                        <input type="date" id="fecha-salida">
+                    </div>
+                    <div class="arrival">
+                        <label for="fecha-llegada">Fecha de llegada:</label><br>
+                        <input type="date" id="fecha-llegada">
+                    </div>
+                </div>
+                <input class="search-button" type="submit" value="Buscar vuelos">
+            </div>
+        </form>
+    </div>
+</body>
+</html>
