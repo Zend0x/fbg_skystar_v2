be6bfe68710f54eea11330ab1feb5e61efdb4aa9
Zend0x <dwknerwow@gmail.com>
2023-05-28 23:20:14 +0200

    feat(flights): improved security

--- src/infraestructura/vuelosAccesoDatos.php
+++ src/infraestructura/vuelosAccesoDatos.php
@@ -3,29 +3,55 @@
 ini_set('display_errors', 1);
 ini_set('html_errors', 1);
 
-class VuelosAccesoDatos{
-    function __construct(){ 
+class VuelosAccesoDatos
+{
+    function __construct()
+    {
     }
 
-    function buscarVuelos($origen,$destino){
-        $conexion=mysqli_connect('localhost','root','12345');
-        if(mysqli_connect_errno()){
-            echo 'Error al conectar a la base de datos.'.mysqli_connect_error();
+    function buscarVuelos($origen, $destino)
+    {
+        $conexion = mysqli_connect('localhost', 'root', '12345');
+        if (mysqli_connect_errno()) {
+            echo 'Error al conectar a la base de datos.' . mysqli_connect_error();
         }
-        mysqli_select_db($conexion,'skystar_airways');
-        $textoConsulta="select flightNum,departure,route,
+        mysqli_select_db($conexion, 'skystar_airways');
+        $textoConsulta = "select flightNum,departure,route,
         (select origin from routes where origin=(?) and destination=(?)) as departureApt,
         (select destination from routes where origin=(?) and destination=(?)) as arrivalApt
         from flights where route=(select id from routes where origin=(?) and destination=(?));";
-        $consulta=mysqli_prepare($conexion,$textoConsulta);
-        $consulta->bind_param("ssssss",$origen,$destino,$origen,$destino,$origen,$destino);
+        $consulta = mysqli_prepare($conexion, $textoConsulta);
+        $consulta->bind_param("ssssss", $origen, $destino, $origen, $destino, $origen, $destino);
         $consulta->execute();
-        $results=$consulta->get_result();
+        $results = $consulta->get_result();
 
-        $aeropuertos=array();
+        $aeropuertos = array();
 
-        while($myrow=$results->fetch_assoc()){
-            array_push($aeropuertos,$myrow);
+        while ($myrow = $results->fetch_assoc()) {
+            array_push($aeropuertos, $myrow);
+        }
+        return $aeropuertos;
+    }
+
+    // funcion de insertarVuelos con parámetros de origen y destino
+
+    function insertarVuelos($origen, $destino)
+    {
+        $conexion = mysqli_connect('localhost', 'root', '12345');
+        if (mysqli_connect_errno()) {
+            echo 'Error al conectar a la base de datos.' . mysqli_connect_error();
+        }
+        mysqli_select_db($conexion, 'skystar_airways');
+        $textoConsulta = "insert into flights (route) values ((select id from routes where origin=(?) and destination=(?)));";
+        $consulta = mysqli_prepare($conexion, $textoConsulta);
+        $consulta->bind_param("ss", $origen, $destino);
+        $consulta->execute();
+        $results = $consulta->get_result();
+
+        $aeropuertos = array();
+
+        while ($myrow = $results->fetch_assoc()) {
+            array_push($aeropuertos, $myrow);
         }
         return $aeropuertos;
     }
--- src/negocio/vuelosReglasNegocio.php
+++ src/negocio/vuelosReglasNegocio.php
@@ -5,48 +5,70 @@ ini_set('html_errors', 1);
 
 require("../infraestructura/vuelosAccesoDatos.php");
 
-class VuelosReglasNegocio{
+class VuelosReglasNegocio
+{
     private $_FLIGHTNUM;
     private $_DEPARTURE;
     private $_ROUTE;
     private $_DEPARTUREAPT;
     private $_ARRIVALAPT;
 
-    function __construct(){
+    function __construct()
+    {
     }
 
-    function init($flightnumber,$departure,$route,$departureApt,$arrivalApt){
-        $this->_FLIGHTNUM=$flightnumber;
-        $this->_DEPARTURE=$departure;
-        $this->_ROUTE=$route;
-        $this->_DEPARTUREAPT=$departureApt;
-        $this->_ARRIVALAPT=$arrivalApt;
+    function init($flightnumber, $departure, $route, $departureApt, $arrivalApt)
+    {
+        $this->_FLIGHTNUM = $flightnumber;
+        $this->_DEPARTURE = $departure;
+        $this->_ROUTE = $route;
+        $this->_DEPARTUREAPT = $departureApt;
+        $this->_ARRIVALAPT = $arrivalApt;
     }
 
-    function getFlightNum(){
+    function getFlightNum()
+    {
         return $this->_FLIGHTNUM;
     }
-    function getDepartureTime(){
+    function getDepartureTime()
+    {
         return $this->_DEPARTURE;
     }
-    function getRoute(){
+    function getRoute()
+    {
         return $this->_ROUTE;
     }
-    function getDeparture(){
+    function getDeparture()
+    {
         return $this->_DEPARTUREAPT;
     }
-    function getArrival(){
+    function getArrival()
+    {
         return $this->_ARRIVALAPT;
     }
 
-    function obtener($origen,$destino){
-        $vuelosDAL=new VuelosAccesoDatos();
-        $rs=$vuelosDAL->buscarVuelos($origen,$destino);
-        $datosVuelos=array();
-        foreach($rs as $vuelo){
-            $vuelosBL=new VuelosReglasNegocio();
-            $vuelosBL->init($vuelo['flightNum'],$vuelo['departure'],$vuelo['route'],$vuelo['departureApt'],$vuelo['arrivalApt']);
-            array_push($datosVuelos,$vuelosBL);
+    function obtener($origen, $destino)
+    {
+        $vuelosDAL = new VuelosAccesoDatos();
+        $rs = $vuelosDAL->buscarVuelos($origen, $destino);
+        $datosVuelos = array();
+        foreach ($rs as $vuelo) {
+            $vuelosBL = new VuelosReglasNegocio();
+            $vuelosBL->init($vuelo['flightNum'], $vuelo['departure'], $vuelo['route'], $vuelo['departureApt'], $vuelo['arrivalApt']);
+            array_push($datosVuelos, $vuelosBL);
+        }
+        return $datosVuelos;
+    }
+
+    function insertar($origen, $destino)
+    {
+        $vuelosDAL = new VuelosAccesoDatos();
+        $rs = $vuelosDAL->insertarVuelos($origen, $destino);
+        $datosVuelos = array();
+        foreach ($rs as $vuelo) {
+            $vuelosBL = new VuelosReglasNegocio();
+            $vuelosBL->init($vuelo['flightNum'], $vuelo['departure'], $vuelo['route'], $vuelo['departureApt'], $vuelo['arrivalApt']);
+            array_push($datosVuelos, $vuelosBL);
         }
         return $datosVuelos;
     }
