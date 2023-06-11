52f58de18ca8ecb0d4bfc691b7aac395a2f21621
Zend0x <dwknerwow@gmail.com>
2023-06-12 01:37:59 +0200

    feat(reservations): added reservation creation logic

--- src/vistas/payment.php
+++ src/vistas/payment.php
@@ -9,9 +9,10 @@ $reservaIda = new reservaReglasNegocio();
 $reservaIda->init($_POST['vuelo-ida'], $_SESSION['username'], $_POST['fecha-ida'], $_POST['precio-ida']);
 $reservaIda->insertar();
 
-$reservaVuelta = new reservaReglasNegocio();
-$reservaVuelta->init($_POST['vuelo-vuelta'], $_SESSION['username'], $_POST['fecha-vuelta'], $_POST['precio-vuelta']);
-$reservaVuelta->insertar();
-
+if ($_POST['vuelo-vuelta'] != null) {
+    $reservaVuelta = new reservaReglasNegocio();
+    $reservaVuelta->init($_POST['vuelo-vuelta'], $_SESSION['username'], $_POST['fecha-vuelta'], $_POST['precio-vuelta']);
+    $reservaVuelta->insertar();
+}
 header("Location: ../vistas/perfilVista.php");
 ?>
