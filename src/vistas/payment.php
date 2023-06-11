6bd9f864d6c048c8af88afe9a8be042b5c449542
Zend0x <dwknerwow@gmail.com>
2023-06-12 01:24:13 +0200

    feat(payment): added insert system for reservations

--- src/vistas/payment.php
+++ src/vistas/payment.php
@@ -1,4 +1,17 @@
 <link rel="shortcut icon" href="../../assets/favicons/favicon.ico" type="image/x-icon">
 <?php
+require "../negocio/reservasReglasNegocio.php";
+session_start();
 
 var_dump($_POST);
+
+$reservaIda = new reservaReglasNegocio();
+$reservaIda->init($_POST['vuelo-ida'], $_SESSION['username'], $_POST['fecha-ida'], $_POST['precio-ida']);
+$reservaIda->insertar();
+
+$reservaVuelta = new reservaReglasNegocio();
+$reservaVuelta->init($_POST['vuelo-vuelta'], $_SESSION['username'], $_POST['fecha-vuelta'], $_POST['precio-vuelta']);
+$reservaVuelta->insertar();
+
+header("Location: ../vistas/perfilVista.php");
+?>
