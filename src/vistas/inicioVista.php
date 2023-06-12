91274f3e6784cbb69b0083b5930bf2c45e39b3af
Zend0x <dwknerwow@gmail.com>
2023-06-12 23:15:04 +0200

    feat(index): added pax selector

--- src/vistas/inicioVista.php
+++ src/vistas/inicioVista.php
@@ -17,9 +17,12 @@
 
     require("../negocio/aeropuertosReglasNegocio.php");
 
-    $aeropuertoBL = new AeropuertosReglasNegocio();
-    $datosAeropuertos = $aeropuertoBL->obtener();
-
+    try {
+        $aeropuertoBL = new AeropuertosReglasNegocio();
+        $datosAeropuertos = $aeropuertoBL->obtener();
+    } catch (Exception $e) {
+        $databaseError = true;
+    }
     session_start();
     if (isset($_SESSION['username'])) {
         $sessionActive = true;
@@ -30,63 +33,78 @@
 </head>
 
 <body>
-    <nav class="navbar">
-        <div class="navbar-center">
-            <span class="navbar-brand"><img src="../../assets/skystar_airways.png" alt="" draggable="false"></span>
-            <div class="search">
-                <form action="buscadorVista.php" method="post" id="search-form">
-                    <div class="trip-type">
-                        <input type="radio" name="trip-type" id="one-way" id="one-way" value="oneway">
-                        <label for="one-way">Solo ida</label>
-                        <input type="radio" name="trip-type" id="round-trip" id="round-trip" value="roundtrip" checked>
-                        <label for="round-trip">Ida y vuelta</label>
-                    </div>
-                    <div class="airport-selectors">
-                        <select name="dep_apt" id="dep_apt" onchange="selectValues('arr_apt',this.value)" class="apt-selector" required>
-                            <option value="null" disabled selected>Seleccione un aeropuerto de salida</option>
-                            <?php
-                            foreach ($datosAeropuertos as $aeropuerto) {
-                                echo '<option value="' . $aeropuerto->getICAO() . '" >' . $aeropuerto->getName() . '</option>';
-                            }
-                            ?>
-                        </select>
-                        <button type="button" onclick="intercambiarValores()" class="interchange-airports"><img src="../../assets/arrow-left-right.svg" alt="change"></button>
-                        <select name="arr_apt" id="arr_apt" onchange="selectValues('dep_apt',this.value)" class="apt-selector" required>
-                            <option value="null" disabled selected>Seleccione un aeropuerto de llegada</option>
-                            <?php
-                            foreach ($datosAeropuertos as $aeropuerto) {
-                                echo '<option value="' . $aeropuerto->getICAO() . '">' . $aeropuerto->getName() . '</option>';
-                            }
-                            ?>
-                        </select>
-                    </div>
-                    <div class="date-selectors-search">
-                        <div class="dep-arrival">
-                            <div class="departure">
-                                <label for="fecha-salida">Fecha de salida:</label><br>
-                                <input type="date" id="fecha-salida" name="fecha-salida" required>
+    <?php
+    if (isset($databaseError)) {
+        echo '<nav class="navbar-error">';
+        echo '<h1 class="error">Ha ocurrido un error en la base de datos</h1>';
+        echo '</nav>';
+    } else {
+        echo '
+            <nav class="navbar">
+                <div class="navbar-center">
+                    <span class="navbar-brand"><img src="../../assets/skystar_airways.png" alt="" draggable="false"></span>
+                    <div class="search">
+                        <form action="buscadorVista.php" method="post" id="search-form">
+                            <div class="trip-type">
+                                <input type="radio" name="trip-type" id="one-way" id="one-way" value="oneway">
+                                <label for="one-way">Solo ida</label>
+                                <input type="radio" name="trip-type" id="round-trip" id="round-trip" value="roundtrip" checked>
+                                <label for="round-trip">Ida y vuelta</label>
                             </div>
-                            <div class="arrival">
-                                <label for="fecha-llegada" id="vuelta-label">Fecha de vuelta:</label><br>
-                                <input type="date" id="fecha-vuelta" name="fecha-vuelta" required>
+                            <div class="airport-selectors">
+                                <select name="dep_apt" id="dep_apt" onchange="selectValues(\'arr_apt\',this.value)" class="apt-selector" required>
+                                    <option value="null" disabled selected>Seleccione un aeropuerto de salida</option>';
+        foreach ($datosAeropuertos as $aeropuerto) {
+            echo '<option value="' . $aeropuerto->getICAO() . '" >' . $aeropuerto->getName() . '</option>';
+        }
+        echo '
+                                </select>
+                                <button type="button" onclick="intercambiarValores()" class="interchange-airports"><img src="../../assets/arrow-left-right.svg" alt="change"></button>
+                                <select name="arr_apt" id="arr_apt" onchange="selectValues(\'dep_apt\',this.value)" class="apt-selector" required>
+                                    <option value="null" disabled selected>Seleccione un aeropuerto de llegada</option>';
+        foreach ($datosAeropuertos as $aeropuerto) {
+            echo '<option value="' . $aeropuerto->getICAO() . '">' . $aeropuerto->getName() . '</option>';
+        }
+        echo '
+                                </select>
                             </div>
-                        </div>
-                        <input id="submit-button" class="search-button" type="submit" value="Buscar vuelos">
+                            <div class="date-selectors-search">
+                                <div class="dep-arrival">
+                                    <div class="departure">
+                                        <label for="fecha-salida">Fecha de salida:</label><br>
+                                        <input type="date" id="fecha-salida" name="fecha-salida" required>
+                                    </div>
+                                    <div class="arrival">
+                                        <label for="fecha-llegada" id="vuelta-label">Fecha de vuelta:</label><br>
+                                        <input type="date" id="fecha-vuelta" name="fecha-vuelta" required>
+                                    </div>
+                                </div>
+                                <div class="pasajeros">
+                                <label for="passengers">Pasajeros:</label><br>
+                                <select name="passengers" id="passengers">
+                                    <option value="1" selected>1</option>
+                                    <option value="2">2 </option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                </select>
+                                </div>
+                                <input id="submit-button" class="search-button" type="submit" value="Buscar vuelos">
+                            </div>
+                        </form>
                     </div>
-                </form>
-            </div>
-        </div>
-        <div class="navbar-right">
-            <?php
-            if (!$sessionActive) {
-                echo '<button class="login-button"><a href="loginVista.php" class="log-in">Iniciar sesión</a></button>';
-            } else if ($sessionActive) {
-                echo '<p>Bienvenido, <a href="perfilVista.php" class="welcome">' . $_SESSION['username'] . '</a></p>';
-                echo '<A href="logoutVista.php" class="log-out">Cerrar sesión</A>';
-            }
-            ?>
-        </div>
-    </nav>
+                </div>
+                <div class="navbar-right">';
+        if (!$sessionActive) {
+            echo '<button class="login-button"><a href="loginVista.php" class="log-in">Iniciar sesión</a></button>';
+        } else if ($sessionActive) {
+            echo '<p>Bienvenido, <a href="perfilVista.php" class="welcome">' . $_SESSION['username'] . '</a></p>';
+            echo '<A href="logoutVista.php" class="log-out">Cerrar sesión</A>';
+        }
+        echo '
+                </div>
+            </nav>';
+    }
+    ?>
     <div class="content">
         <h1 class="content-title">¿A dónde quieres volar?</h1>
         <div class="grid-container">
